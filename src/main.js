0f27a122c54a0baf92f89634ceadd0e28113938c
Nikolascerbakov <scerbakovnikola@gmail.com>
2025-07-28 17:26:12 +0200

    added nav and toggle

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
+document.addEventListener('DOMContentLoaded', () => {
+  // Select all dropdown toggles
+  const dropdownToggles = document.querySelectorAll('.dropdown-toggle')
+  
+  dropdownToggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      const dropdownMenu = toggle.nextElementSibling
+
+      if(dropdownMenu.classList.contains('hidden')) {
+        document.querySelectorAll('.dropdown-menu').forEach(menu => {
+          menu.classList.add('hidden')
+        })
+
+        dropdownMenu.classList.remove('hidden')
+      } else {
+        dropdownMenu.classList.add('hidden')
+      }
+    })
+  })
+
+  // mobile menu toggle
+
+  const mobileMenuButton = document.querySelector('.mobile-menu-button')
+  const mobileMenu = document.querySelector('.nav-div__navigation')
+
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+  })
+
+})
 
